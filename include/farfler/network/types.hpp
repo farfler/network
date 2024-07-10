e8d7ac4aa3459aba6344b26db56d68550ce5147c
Daniel Bacsur <contact@danielbacsur.com>
2024-07-09 23:25:33 -0700

    feat: add Number template class for numeric types

--- include/farfler/network/types.hpp
+++ include/farfler/network/types.hpp
@@ -1,3 +1,32 @@
 #pragma once
 
-namespace farfler::network {}
+#include <cstdint>
+#include <vector>
+
+namespace farfler::network {
+
+template <typename T>
+class Number {
+ public:
+  explicit Number(T value = 0);
+  static std::vector<char> Serialize(const T& msg);
+  static std::vector<char> Serialize(const T& msg, std::vector<char>& packet);
+  static T Deserialize(std::vector<char>& packet);
+  static T Deserialize(std::vector<char>& packet, T& msg);
+
+  T value_;
+};
+
+using Int8 = Number<int8_t>;
+using Int16 = Number<int16_t>;
+using Int32 = Number<int32_t>;
+using Int64 = Number<int64_t>;
+using UInt8 = Number<uint8_t>;
+using UInt16 = Number<uint16_t>;
+using UInt32 = Number<uint32_t>;
+using UInt64 = Number<uint64_t>;
+using Float32 = Number<float>;
+using Float64 = Number<double>;
+using Boolean = Number<bool>;
+
+}  // namespace farfler::network
--- src/farfler/network/types.cpp
+++ src/farfler/network/types.cpp
@@ -1,3 +1,50 @@
+#include <cstring>
 #include <farfler/network/types.hpp>
 
-namespace farfler::network {}
+namespace farfler::network {
+
+template <typename T>
+Number<T>::Number(T value) : value_(value) {}
+
+template <typename T>
+std::vector<char> Number<T>::Serialize(const T &msg) {
+  std::vector<char> packet;
+  Serialize(msg, packet);
+  return packet;
+}
+
+template <typename T>
+std::vector<char> Number<T>::Serialize(const T &msg,
+                                       std::vector<char> &packet) {
+  packet.insert(packet.end(), reinterpret_cast<const char *>(&msg),
+                reinterpret_cast<const char *>(&msg) + sizeof(T));
+  return packet;
+}
+
+template <typename T>
+T Number<T>::Deserialize(std::vector<char> &packet) {
+  T msg;
+  Deserialize(packet, msg);
+  return msg;
+}
+
+template <typename T>
+T Number<T>::Deserialize(std::vector<char> &packet, T &msg) {
+  memcpy(&msg, packet.data(), sizeof(T));
+  packet.erase(packet.begin(), packet.begin() + sizeof(T));
+  return msg;
+}
+
+template class Number<int8_t>;
+template class Number<int16_t>;
+template class Number<int32_t>;
+template class Number<int64_t>;
+template class Number<uint8_t>;
+template class Number<uint16_t>;
+template class Number<uint32_t>;
+template class Number<uint64_t>;
+template class Number<float>;
+template class Number<double>;
+template class Number<bool>;
+
+}  // namespace farfler::network
